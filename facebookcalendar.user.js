a84343d559f5e0dc4ca725856ade42b6f90fc3d1
Jake Stothard <stothardj@gmail.com>
2011-05-06 21:59:47 -0700

    Seperate table generating code

--- facebookcalendar.user.js
+++ facebookcalendar.user.js
@@ -16,19 +16,22 @@ $('head').append(
   border-color: #000000; \
   border-radius: 5px; \
   -moz-border-radius: 5px; \
+  padding: 5px;
 } \
 </style>'
 );
 $('#pagelet_eventbox').empty();
-$('#pagelet_eventbox').append(
-'<div id="facebook_calendar"> \
-<h1>Facebook Calendar</h1> \
-<h2>Month</h2> \
-1 2 3 4 5 6 7<br/> \
+var calendarTitle = $('<h1>Facebook Calendar</h1>');
+var calendarMonth = $('<h2>Month</h2>');
+var calendarTable = $(
+'1 2 3 4 5 6 7<br/> \
 8 9 10 11 12 13<br/> \
 14 15 16 17 18 19<br/> \
 20 21 22 23 24 25<br/> \
-26 27 28 29 30 31<br/> \
-</div>'
+26 27 28 29 30 31<br/>'
 );
 
+$('#pagelet_eventbox').append(calendarTitle);
+calendarTitle.after(calendarMonth);
+calendarMonth.after(calendarTable);
+$('#pagelet_eventbox').wrap('<div id="facebook_calendar" />');
