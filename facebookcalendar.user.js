7c3e8b343a294f31dcdbb0f0118a35d09960319d
Jake Stothard <stothardj@gmail.com>
2011-05-06 21:03:27 -0700

    Add gitignore and facebookcalendar base

---
+++ facebookcalendar.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           FacebookCalendar
+// @namespace      https://github.com/stothardj/FacebookCalendar
+// @description    Calendar
+// @include        http://www.facebook.com/
+// @require http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.min.js
+// @version        0.1
+// ==/UserScript==
+
+$('#pagelet_eventbox').empty();
+$('#pagelet_eventbox').append("Hello World");
+
